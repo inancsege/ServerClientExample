c8b9dcc3f3c9a5a23a199bbda7c53ce13d5abb26
serhat ege inanç <serhatege2000@gmail.com>
2022-09-13 21:30:17 +0300

    Some of the replacements.

--- src/SocketClientExample.java
+++ src/SocketClientExample.java
@@ -3,22 +3,21 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
-import java.net.UnknownHostException;
 
 public class SocketClientExample {
-    public static void main(String[] args) throws UnknownHostException, IOException, ClassNotFoundException, InterruptedException{
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException{
         //get the localhost IP address, if server is running on some other IP, you need to use that
         InetAddress host = InetAddress.getLocalHost();
-        Socket socket = null;
-        ObjectOutputStream oos = null;
-        ObjectInputStream ois = null;
-        for(int i=0; i<5;i++){
+        Socket socket;
+        ObjectOutputStream oos;
+        ObjectInputStream ois;
+        for(int i=0; i<10;i++){
             //establish socket connection to server
             socket = new Socket(host.getHostName(), 9876);
             //write to socket using ObjectOutputStream
             oos = new ObjectOutputStream(socket.getOutputStream());
             System.out.println("Sending request to Socket Server");
-            if(i==4)oos.writeObject("exit");
+            if(i==9)oos.writeObject("exit");
             else oos.writeObject(""+i);
             //read the server response message
             ois = new ObjectInputStream(socket.getInputStream());
--- src/SocketServerExample.java
+++ src/SocketServerExample.java
@@ -5,14 +5,13 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class SocketServerExample {
-    //static ServerSocket variable
-    private static ServerSocket server;
-    //socket server port on which it will listen
-    private static int port = 9876;
 
-    public static void main(String args[]) throws IOException, ClassNotFoundException{
+    public static void main(String[] args) throws IOException, ClassNotFoundException{
         //create the socket server object
-        server = new ServerSocket(port);
+        //static ServerSocket variable
+        //socket server port on which it will listen
+        int port = 9876;
+        ServerSocket server = new ServerSocket(port);
         //keep listens indefinitely until receives 'exit' call or program terminates
         while(true){
             System.out.println("Waiting for the client request");
